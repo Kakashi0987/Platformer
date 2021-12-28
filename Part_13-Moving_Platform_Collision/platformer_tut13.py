7fa046468ef318b978f0c38ca426104658696d9d
Kakashi0987 <94703527+Kakashi0987@users.noreply.github.com>
2021-12-28 03:16:45 -0500

    Update platformer_tut13.py

--- Part_13-Moving_Platform_Collision/platformer_tut13.py
+++ Part_13-Moving_Platform_Collision/platformer_tut13.py
@@ -44,16 +44,6 @@ restart_img = pygame.image.load('img/restart_btn.png')
 start_img = pygame.image.load('img/start_btn.png')
 exit_img = pygame.image.load('img/exit_btn.png')
 
-#load sounds
-pygame.mixer.music.load('img/music.wav')
-pygame.mixer.music.play(-1, 0.0, 5000)
-coin_fx = pygame.mixer.Sound('img/coin.wav')
-coin_fx.set_volume(0.5)
-jump_fx = pygame.mixer.Sound('img/jump.wav')
-jump_fx.set_volume(0.5)
-game_over_fx = pygame.mixer.Sound('img/game_over.wav')
-game_over_fx.set_volume(0.5)
-
 
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
